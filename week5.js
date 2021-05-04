2fda35ef915debea48c2db9aaf85c500f50c390d
swookkim <sangwook.kim@kellogg.northwestern.edu>
2021-05-03 20:49:21 -0500

    1

--- week5.js
+++ week5.js
@@ -88,7 +88,7 @@ window.addEventListener('DOMContentLoaded', async function() {
           <div class ="text-center space-y-8">
           <img src="https:${dailyForecast.forecastday[i].day.condition.icon}" class="mx-auto">
           <h1 class="text-2xl text-bold text-gray-500">${dailyForecast.forecastday[i].date}</h1>
-          <h2 class="text-xl">High ${dailyForecast.forecastday[i].day.maxtemp_f}° – ${dailyForecast.forecastday[i].day.mintemp_f}°</h2>
+          <h2 class="text-xl">High ${dailyForecast.forecastday[i].day.maxtemp_f}° – Low ${dailyForecast.forecastday[i].day.mintemp_f}°</h2>
           <p class="text-gray-500">${dailyForecast.forecastday[i].day.condition.text}</h1>
         </div>`
         )      
