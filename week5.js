efa31a71f3a96da9fd06df92051556d52d5ec601
swookkim <sangwook.kim@kellogg.northwestern.edu>
2021-05-03 19:05:22 -0500

    1

--- week5.js
+++ week5.js
@@ -18,24 +18,49 @@ window.addEventListener('DOMContentLoaded', async function() {
   let getWeatherButton = document.querySelector(`.get-weather`)
 
   // When the "get weather" button is clicked:
-
+  getWeatherButton.addEventListener(`click`, async function(event) {
     // - Ignore the default behavior of the button
+    event.preventDefault()
 
     // - Get a reference to the element containing the user-entered location
+    let locationInput = document.querySelector(#location)
 
     // - Get the user-entered location from the element's value
+    let location = locationInput.value
 
     // - Check to see if the user entered anything; if so:
+    if (location.length > 0) {
 
       // - Construct a URL to call the WeatherAPI.com API
-
+      let url = `https://api.weatherapi.com/v1/forecast.json?key=2d0b630257e44b2795b154134212704LIVE&q=${location}&days=3`
+    
       // - Fetch the url, wait for a response, store the response in memory
+      let response = await fetch(url)
 
       // - Ask for the json-formatted data from the response, wait for the data, store it in memory
+      let json = await response.json()
 
       // - Write the json-formatted data to the JavaScript console
-
+      console.log(json)
+      
       // - Store the interpreted location, current weather conditions, the forecast as three separate variables
+      let interpretedLocation = json.location
+      let currentWeather = json.current
+      let dailyForecast = json.forecast
 
-      // - Continue the recipe yourself!
+      // - Store a reference to the current element
+      let element = document.querySelector(`.current`)
+
+      // - Fill the current element with the location and current weather condition
+      
+
+
+    
+    
+    }
+
+  }
+  )
+
+    
 })
